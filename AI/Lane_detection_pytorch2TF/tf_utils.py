48114324de02a067822ac6ce3d911aa346e6be92
unknown <kts8559@gmail.com>
2021-07-05 17:35:14 +0900

    Add 20210705 TIL [AI] [Lane Detection] [ConvLSTMCell]

--- AI/Lane_detection_pytorch2TF/tf_utils.py
+++ AI/Lane_detection_pytorch2TF/tf_utils.py
@@ -85,3 +85,57 @@ class outconv(tf.keras.layers.Layer):
         x = self.conv(x)
         # print("Shape after  conv:", x.shape)
         return x
+
+## TF ConvLSTMCell
+class ConvLSTMCell(tf.keras.layers.Layer):
+    def __init__(self, input_size, input_dim, hidden_dim, kernel_size, bias):
+        """
+        Initialize ConvLSTM cell.
+
+        Parameters
+        ----------
+        input_size: (int, int)
+            Height and width of input tensor as (height, width).
+        input_dim: int
+            Number of channels of input tensor.
+        hidden_dim: int
+            Number of channels of hidden state.
+        kernel_size: (int, int)
+            Size of the convolutional kernel.
+        bias: bool
+            Whether or not to add the bias.
+        """
+        
+        super(ConvLSTMCell, self).__init__()
+        
+        self.height, self.width = input_size
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+
+        self.kernel_size = kernel_size
+        self.padding = kernel_size[0] // 2, kernel_size[1] // 2
+        self.bias = bias
+
+        self.conv = Conv2D(filters=4*self.hidden_dim, kernel_size=self.kernel_size, padding=self.padding, use_bias=self.bias)
+
+    def call(self, input_tensor, cur_state):
+        h_cur, c_cur = cur_state
+
+        combined = tf.concat([input_tensor, h_cur], axis=-1)
+
+        combined_conv = self.conv(combined)
+
+        cc_i, cc_f, cc_o, cc_g = tf.split(value=combined_conv, num_or_size_splits=self.hidden_dim, axis=1)
+        i = tf.sigmoid(cc_i)
+        f = tf.sigmoid(cc_f)
+        o = tf.sigmoid(cc_o)
+        g = tf.tanh(cc_g)
+
+        c_next = f * c_cur + i * g
+        h_next = o * tf.tanh(c_next)
+        print(h_next.shape, c_next.shape)
+        return h_next, c_next
+    
+    def init_hidden(self, batch_size):
+        return (tf.zeros([batch_size, self.hidden_dim, self.height, self.width], dtype=tf.float32),
+                tf.zeros([batch_size, self.hidden_dim, self.height, self.width], dtype=tf.float32))
